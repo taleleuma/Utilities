7570a6adf4781f47e3c3f72ac40c7251eb936d87
tumayahoo <16799155+taleleuma@users.noreply.github.com>
2018-04-17 16:28:05 -0400

    initial checkin
    Collects folder and subfolder sizes, number of files etc. and inserts into DB for further actions

---
+++ CollectFolderSizesIntoDB/MainForm.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace CollectFolderSizesIntoDB
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Submit = new System.Windows.Forms.Button();
+            this.msg = new System.Windows.Forms.Label();
+            this.txtRootFolder = new System.Windows.Forms.TextBox();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Root Folder:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // Submit
+            // 
+            this.Submit.Location = new System.Drawing.Point(174, 59);
+            this.Submit.Name = "Submit";
+            this.Submit.Size = new System.Drawing.Size(204, 23);
+            this.Submit.TabIndex = 4;
+            this.Submit.Text = "Collect Root and Subfolders Sizes";
+            this.Submit.UseVisualStyleBackColor = true;
+            this.Submit.Click += new System.EventHandler(this.CollectFolderSizesIntoDB_Click);
+            // 
+            // msg
+            // 
+            this.msg.AutoSize = true;
+            this.msg.Location = new System.Drawing.Point(47, 177);
+            this.msg.Name = "msg";
+            this.msg.Size = new System.Drawing.Size(0, 13);
+            this.msg.TabIndex = 5;
+            // 
+            // txtRootFolder
+            // 
+            this.txtRootFolder.Location = new System.Drawing.Point(86, 21);
+            this.txtRootFolder.Name = "txtRootFolder";
+            this.txtRootFolder.Size = new System.Drawing.Size(424, 20);
+            this.txtRootFolder.TabIndex = 6;
+            this.txtRootFolder.Text = "D:\\Reports";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.ForeColor = System.Drawing.Color.Blue;
+            this.lblStatus.Location = new System.Drawing.Point(44, 102);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(0, 13);
+            this.lblStatus.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(455, 59);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(172, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Delete Preview QA folders";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Delete_Preview_QA_Folders_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(712, 141);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.txtRootFolder);
+            this.Controls.Add(this.msg);
+            this.Controls.Add(this.Submit);
+            this.Controls.Add(this.label1);
+            this.Name = "MainForm";
+            this.Text = "PCI ReEncrypt Utility";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Submit;
+        private System.Windows.Forms.Label msg;
+        private System.Windows.Forms.TextBox txtRootFolder;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ CollectFolderSizesIntoDB/MainForm.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.IO;
+using System.Windows.Forms;
+using System.Configuration;
+using static CollectFolderSizesIntoDB.RelayAccountOnlineDataSet;
+
+namespace CollectFolderSizesIntoDB
+{
+    public partial class MainForm : Form
+    {
+        private DateTime _now;
+        private int secondsToRefresh = 10;
+        private int _foldersCount = 0;
+
+        public MainForm()
+        {
+            //msg.Visible = false;
+            InitializeComponent();
+        }
+
+        private void CollectFolderSizesIntoDB_Click(object sender, EventArgs e)  //start processing
+        {
+            lblStatus.Text = "Processing....";
+            Application.DoEvents();
+
+            string rootFolder = txtRootFolder.Text;
+
+            if (!Directory.Exists(rootFolder))
+            {
+                lblStatus.Text = " Directory does not exist";
+                return;
+            }
+
+            FolderSizeDataTable datatable = new FolderSizeDataTable();
+            _now = DateTime.Now.AddSeconds(secondsToRefresh);
+            _foldersCount = 0;
+
+            GetFolderInfo(rootFolder, ref datatable);   //for root folder
+
+            AdapterUpdate(datatable);
+
+            lblStatus.Text = "Done. Please check DBA.FolderSize database table";
+        }
+
+
+        private void GetFolderInfo(string folder, ref FolderSizeDataTable datatable)
+        {
+            _foldersCount++;
+            try
+            {
+                Int64 fileSize = 0;
+                int fileCount = 0;
+                foreach (FileInfo fi in new DirectoryInfo(folder).EnumerateFiles("*", SearchOption.TopDirectoryOnly))
+                {
+                    fileSize += fi.Length;
+                    fileCount++;
+                }
+
+                datatable.AddFolderSizeRow(DateTime.Now, folder, fileCount, (int)(fileSize/1024));
+
+                foreach (string childfolder in Directory.EnumerateDirectories(folder))    //for subfolders
+                {
+                    GetFolderInfo(childfolder, ref datatable);    //iterate
+                }
+
+                if (DateTime.Now > _now)
+                {
+                    _now = DateTime.Now.AddSeconds(secondsToRefresh);
+                    AdapterUpdate(datatable);
+                    datatable = new FolderSizeDataTable();
+                    lblStatus.Text = "Processing.... " + string.Format("Done with {0} folders. Please wait..", _foldersCount);
+                    Application.DoEvents();
+                }
+
+            }
+            catch (Exception Ex)
+            {
+                lblStatus.Text = Ex.Message;
+                Application.DoEvents();
+            }
+
+        }
+
+        private void Delete_Preview_QA_Folders(string rootFolder, ref FolderSizeDataTable datatable)
+        {
+            try
+            {
+                foreach (string clientfolder in Directory.EnumerateDirectories(rootFolder))    //for client level folders
+                {
+                    foreach (string environmentFolder in Directory.EnumerateDirectories(clientfolder))    //for environment folders
+                    {
+                        if (environmentFolder.EndsWith("Preview") || environmentFolder.EndsWith("QA"))
+                        {
+                            Directory.Delete(environmentFolder, true);
+                            datatable.AddFolderSizeRow(DateTime.Now, environmentFolder, 0, 0);
+                            _foldersCount++;
+
+                            if (DateTime.Now > _now)
+                            {
+                                _now = DateTime.Now.AddSeconds(secondsToRefresh);
+                                AdapterUpdate(datatable);
+                                datatable = new FolderSizeDataTable();
+                                lblStatus.Text = "Processing.... " + string.Format("Done with {0} folders. Please wait..", _foldersCount);
+                                Application.DoEvents();
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception Ex)
+            {
+                lblStatus.Text = Ex.Message;
+                Application.DoEvents();
+            }
+
+        }
+        private void AdapterUpdate(FolderSizeDataTable dt)
+        {
+            DataTable dataTable = dt;
+
+            using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["PatientCompassConnectionString"].ConnectionString))
+            {
+                SqlDataAdapter adapter = new SqlDataAdapter();
+
+                // Set the INSERT command and parameter.
+                adapter.InsertCommand = new SqlCommand(
+                    "INSERT INTO [DBA].[FolderSize] (GeneratedAt, FullFolderPath,NumberOfFiles,SizeInKBs) VALUES (@GeneratedAt, @FullFolderPath,@NumberOfFiles,@SizeInKBs);", connection);
+
+                adapter.InsertCommand.Parameters.Add("@GeneratedAt", SqlDbType.DateTime, 50, "GeneratedAt");
+                adapter.InsertCommand.Parameters.Add("@FullFolderPath", SqlDbType.NVarChar, 1000, "FullFolderPath");
+                adapter.InsertCommand.Parameters.Add("@NumberOfFiles", SqlDbType.Int, 4, "NumberOfFiles");
+                adapter.InsertCommand.Parameters.Add("@SizeInKBs", SqlDbType.Int, 4, "SizeInKBs");
+
+                adapter.InsertCommand.UpdatedRowSource = UpdateRowSource.None;
+
+                // Set the batch size.
+                adapter.UpdateBatchSize = 100000;
+
+                try {
+                    adapter.Update(dataTable);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void Delete_Preview_QA_Folders_Click(object sender, EventArgs e)
+        {
+            lblStatus.Text = "Processing....";
+            Application.DoEvents();
+
+            string rootFolder = txtRootFolder.Text;
+
+            if (!Directory.Exists(rootFolder))
+            {
+                lblStatus.Text = " Directory does not exist";
+                return;
+            }
+
+            FolderSizeDataTable datatable = new FolderSizeDataTable();
+            _now = DateTime.Now.AddSeconds(secondsToRefresh);
+            _foldersCount = 0;
+
+            Delete_Preview_QA_Folders(rootFolder, ref datatable);   //for root folder
+
+            AdapterUpdate(datatable);
+
+            lblStatus.Text = "Done. Please check DBA.FolderSize database table";
+        }
+    }
+}
